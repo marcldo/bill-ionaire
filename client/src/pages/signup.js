11f16cc51082c23ba0a6c5a0fa894a3ebdc1469d
gitMarcldo <marcldo@users.noreply.github.com>
2019-11-09 22:53:29 -0500

    added nav bar and completed signup functionality

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -1,17 +1,35 @@
 import React, { Component } from "react";
 import API from "../utils/API";
-import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
-import { List, ListItem } from "../components/List";
 import { Input, TextArea, FormBtn } from "../components/Form";
 
 class Signup extends Component {
   state = {
-    username: "",
-    password1: "",
-    password2: "",
+    email: "",
+    password: ""
 
-  }
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    console.log("clicked");
+    event.preventDefault();
+    if (this.state.email && this.state.password) {
+      API.saveUser({
+        email: this.state.email,
+        password: this.state.password
+      })
+        .then(console.log("User Created"))
+        .catch(err => console.log(err));
+    }
+  };
+
   render() {
     return (
       <Container fluid>
@@ -19,23 +37,28 @@ class Signup extends Component {
           <Col size="md-6">
             <form>
               <Input
-                value={this.state.username}
+                value={this.state.email}
                 onChange={this.handleInputChange}
-                name="username"
-                placeholder="Username (required)"
+                name="email"
+                placeholder="Email (required)"
               />
               <Input
                 value={this.state.password}
                 onChange={this.handleInputChange}
-                name="username"
-                placeholder="Username (required)"
+                name="password"
+                placeholder="Password (required)"
+                type="password"
               />
-
+              <FormBtn onClick={this.handleFormSubmit}>
+                Sign Up
+              </FormBtn>
             </form>
           </Col>
         </Row>
 
       </Container>
-    )
+    );
   }
-}
+};
+
+export default Signup;
