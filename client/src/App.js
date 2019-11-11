ad3de8c1646da39ce71901cfb19e4dd0f7f68f21
gitMarcldo <marcldo@users.noreply.github.com>
2019-11-11 14:48:15 -0500

    changed signup.js to lowercase, added git ignore to client folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
-import Signup from "./pages/Signup";
+import Signup from "./pages/signup";
 import Login from "./pages/Login";
 import "./App.css";
 
