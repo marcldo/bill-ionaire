8a73967f03e6e528dffecbf79ee84641cfff818a
gitMarcldo <marcldo@users.noreply.github.com>
2019-11-14 21:59:25 -0500

    created models; bill & recurBill with associations

---
+++ models/recurBill.js
@@ -0,0 +1,37 @@
+module.exports = function (sequelize, DataTypes) {
+  const RecurBill = sequelize.define("RecurBill", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    amount: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      len: [1]
+    },
+    frequency: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      len: [1]
+    },
+    startDate: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    }
+  });
+
+  RecurBill.associate = function (models) {
+    models.RecurBill.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+    models.RecurBill.hasMany(models.Bill, {
+      onDelete: "cascade"
+    });
+  };
+  return RecurBill;
+}
