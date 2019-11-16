fa92953d47c786dc5e9a6031c8fecdac13528f3b
gitMarcldo <marcldo@users.noreply.github.com>
2019-11-15 21:50:56 -0500

    created api file and get and post

---
+++ routes/api/recurBill-api-routes.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const router = express.Router();
+
+const db = require("../../models")
+
+// GET route for getting all of the bills
+router.get("/api/posts", function (req, res) {
+  var query = {};
+  if (req.query.UserId) {
+    query.UserId = req.query.UserId;
+  }
+  db.RecurBills.findAll({
+    where: query
+  }).then(function (dbRecurBill) {
+    res.json(dbRecurBill);
+  });
+});
+
+//GET route for retrieving a single recurring bill
+router.get("/api/recurbills/:id", function (req, res) {
+  db.RecurBills.findOne({
+    where: {
+      id: req.params.id
+    }
+  }).then(function (dbRecurBill) {
+    console.log(dbRecurBill);
+    res.json(dbRecurBill);
+  });
+});
+
+
+//POST route for saving a new recurring bill
+router.post("/api/recurbills/:id", function (req, res) {
+  db.RecurBills.create(req.body)
+    .then(function (dbRecurBill) {
+      res.json(dbRecurBill);
+    });
+});
+
+// DELETE route for deleting recurring bills
+router.delete("/api/recurbills/:id", function (req, res) {
+  db.RecurBills.destroy({
+    //////////
+  })
+})
+
+//PUT route for updating recurring 
