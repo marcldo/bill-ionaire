f3bb76ca76ffdaa989f1b2d1e7e539bb784f3d1f
Christine Hong <honglikehk@github.com>
2019-11-16 14:57:48 -0500

    added table to the history page

--- client/src/pages/History.js
+++ client/src/pages/History.js
@@ -1,3 +1,91 @@
-import React from 'react'
-const History = () => <h1>History</h1>
-export default History
+import React from "react";
+import Table from "react-bootstrap/Table";
+import Container from "../components/Container";
+import Row from "../components/Row";
+import Col from "../components/Col";
+
+function History() {
+  return (
+    <div>
+      <Container style={{ marginTop: 30 }}>
+        <h1>Bill Payments History</h1>
+        <Row>
+          <Col size="md-12">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+              aliquet diam tortor, id consequat mauris ullamcorper eu. Orci
+              varius natoque penatibus et magnis dis parturient montes, nascetur
+              ridiculus mus. Pellentesque et dui id justo finibus sollicitudin
+              at et metus. Ut feugiat tellus nec metus commodo, sed suscipit
+              nisi gravida. Duis eget vestibulum quam, ut porttitor sem. Donec
+              sagittis mi sollicitudin turpis semper, et interdum risus
+              lobortis. Vestibulum suscipit nunc non egestas tristique. Proin
+              hendrerit efficitur malesuada. Mauris lorem urna, sodales accumsan
+              quam non, tristique tempor erat. Nullam non sem facilisis, tempus
+              tortor sit amet, volutpat nisl. Ut et turpis non nunc maximus
+              mollis a vitae tortor. Pellentesque mattis risus ac quam laoreet
+              cursus. Praesent suscipit orci neque, vestibulum tincidunt augue
+              tincidunt non. Duis consequat mattis tortor vitae mattis.
+            </p>
+            <Table responsive>
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Bill Company Name:</th>
+                  <th>Amount:</th>
+                  <th>Due Date:</th>
+                  <th>Paid:</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Table cell</td>
+                  <td>Table cell</td>
+                  <td>Table cell</td>
+                  <td>Table cell</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Table cell</td>
+                  <td>Table cell</td>
+                  <td>Table cell</td>
+                  <td>Table cell</td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td>Table cell</td>
+                  <td>Table cell</td>
+                  <td>Table cell</td>
+                  <td>Table cell</td>
+                </tr>
+                <tr>
+                  <td>4</td>
+                  <td>Table cell</td>
+                  <td>Table cell</td>
+                  <td>Table cell</td>
+                  <td>Table cell</td>
+                </tr>
+                <tr>
+                  <td>5</td>
+                  <td>Table cell</td>
+                  <td>Table cell</td>
+                  <td>Table cell</td>
+                  <td>Table cell</td>
+                </tr>
+                <tr>
+                  <td>6</td>
+                  <td>Table cell</td>
+                  <td>Table cell</td>
+                  <td>Table cell</td>
+                  <td>Table cell</td>
+                </tr>
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+export default History;
--- server.js
+++ server.js
@@ -31,13 +31,13 @@ app.use(require("./routes/api/recurBill-api-routes"));
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function (req, res) {
+app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(function () {
-  app.listen(PORT, function () {
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
