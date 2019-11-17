b184be328d855b1831821e91a0a48473a010dbfb
gitMarcldo <marcldo@users.noreply.github.com>
2019-11-17 14:05:28 -0500

    created column structure

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,3 +1,67 @@
-import React from 'react'
-const Dashboard = () => <h1>Dashboard</h1>
+import React, { Component } from 'react';
+import API from "../utils/API";
+import { Col, Row, Container } from "../components/Grid";
+import { List, ListItem } from "../components/List";
+
+
+
+class Dashboard extends Component {
+  state = {
+    dueBills: [],
+    paidBills: [],
+    overdueBills: []
+  };
+
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col size="md-4 sm-12">
+            {this.state.dueBills.length ? (
+              <List>
+                {this.state.dueBills.map(dueBill => (
+                  <ListItem key={dueBill.id}>
+                    <strong>
+                      Name: {dueBill.name} Amount: {dueBill.amount} Due Date: {dueBill.dueDate}
+                    </strong>
+                  </ListItem>
+                ))}
+              </List>
+            ) :
+              (<h3>No Bills Are Due</h3>)}
+          </Col>
+          <Col size="md-4 sm-12">
+            {this.state.dueBills.length ? (
+              <List>
+                {this.state.dueBills.map(dueBill => (
+                  <ListItem key={dueBill.id}>
+                    <strong>
+                      Name: {dueBill.name} Amount: {dueBill.amount} Due Date: {dueBill.dueDate}
+                    </strong>
+                  </ListItem>
+                ))}
+              </List>
+            ) :
+              (<h3>No Bills Are Due</h3>)}
+          </Col>
+          <Col size="md-4 sm-12">
+            {this.state.dueBills.length ? (
+              <List>
+                {this.state.dueBills.map(dueBill => (
+                  <ListItem key={dueBill.id}>
+                    <strong>
+                      Name: {dueBill.name} Amount: {dueBill.amount} Due Date: {dueBill.dueDate}
+                    </strong>
+                  </ListItem>
+                ))}
+              </List>
+            ) :
+              (<h3>No Bills Are Due</h3>)}
+          </Col>
+        </Row>
+      </Container >
+    )
+  }
+
+}
 export default Dashboard
