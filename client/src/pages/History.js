1deb7895fcfd04d29082838670d73c0b7135000c
Christine Hong <honglikehk@github.com>
2019-11-18 20:56:24 -0500

    finished history

--- client/src/pages/History.js
+++ client/src/pages/History.js
@@ -1,91 +1,57 @@
 import React from "react";
-import Table from "react-bootstrap/Table";
-import Container from "../components/Container";
-import Row from "../components/Row";
-import Col from "../components/Col";
+import API from "../utils/API";
+import { Col, Row, Container, Table } from "../components/Grid";
 
-function History() {
-  return (
-    <div>
-      <Container style={{ marginTop: 30 }}>
-        <h1>Bill Payments History</h1>
-        <Row>
-          <Col size="md-12">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-              aliquet diam tortor, id consequat mauris ullamcorper eu. Orci
-              varius natoque penatibus et magnis dis parturient montes, nascetur
-              ridiculus mus. Pellentesque et dui id justo finibus sollicitudin
-              at et metus. Ut feugiat tellus nec metus commodo, sed suscipit
-              nisi gravida. Duis eget vestibulum quam, ut porttitor sem. Donec
-              sagittis mi sollicitudin turpis semper, et interdum risus
-              lobortis. Vestibulum suscipit nunc non egestas tristique. Proin
-              hendrerit efficitur malesuada. Mauris lorem urna, sodales accumsan
-              quam non, tristique tempor erat. Nullam non sem facilisis, tempus
-              tortor sit amet, volutpat nisl. Ut et turpis non nunc maximus
-              mollis a vitae tortor. Pellentesque mattis risus ac quam laoreet
-              cursus. Praesent suscipit orci neque, vestibulum tincidunt augue
-              tincidunt non. Duis consequat mattis tortor vitae mattis.
-            </p>
-            <Table responsive>
-              <thead>
-                <tr>
-                  <th></th>
-                  <th>Bill Company Name:</th>
-                  <th>Amount:</th>
-                  <th>Due Date:</th>
-                  <th>Paid:</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>Table cell</td>
-                  <td>Table cell</td>
-                  <td>Table cell</td>
-                  <td>Table cell</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Table cell</td>
-                  <td>Table cell</td>
-                  <td>Table cell</td>
-                  <td>Table cell</td>
-                </tr>
-                <tr>
-                  <td>3</td>
-                  <td>Table cell</td>
-                  <td>Table cell</td>
-                  <td>Table cell</td>
-                  <td>Table cell</td>
-                </tr>
-                <tr>
-                  <td>4</td>
-                  <td>Table cell</td>
-                  <td>Table cell</td>
-                  <td>Table cell</td>
-                  <td>Table cell</td>
-                </tr>
-                <tr>
-                  <td>5</td>
-                  <td>Table cell</td>
-                  <td>Table cell</td>
-                  <td>Table cell</td>
-                  <td>Table cell</td>
-                </tr>
-                <tr>
-                  <td>6</td>
-                  <td>Table cell</td>
-                  <td>Table cell</td>
-                  <td>Table cell</td>
-                  <td>Table cell</td>
-                </tr>
-              </tbody>
-            </Table>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+class History extends React.Component {
+  state = {
+    bills: []
+  };
+
+  componentDidMount() {
+    this.loadBills();
+  }
+
+  loadBills = () => {
+    API.getBills()
+      .then(res => this.setState({ bills: res.data }))
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    return (
+      <div>
+        <Container style={{ marginTop: 30 }}>
+          <h1>Bill Payments History</h1>
+          <Row>
+            <Col size="md-12">
+              <Table responsive>
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th>Bill Company Name:</th>
+                    <th>Amount:</th>
+                    <th>Due Date:</th>
+                    <th>Paid:</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.state.bills.map(bill => (
+                    <tr>
+                      <td>{bill.id}</td>
+                      <td>{bill.name}</td>
+                      <td>{bill.amount}</td>
+                      <td>{bill.dueDate}</td>
+                      <td>{bill.paid}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
+
 export default History;
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,23 +1,45 @@
 import axios from "axios";
 
 export default {
-	saveUser: function (userData) {
-		return axios.post("/api/signup", userData);
-	},
-	loginUser: function (email, password) {
-		return axios.post("/api/login", {
-			email,
-			password
-		})
-			.then(function () {
-				console.log("logged in")
-				window.location.replace("/members");
-			})
-			.catch(function (err) {
-				console.log(err);
-			});
-	},
-	getUser: function () {
-		return axios.get("/api/user_data")
-	}
-}
+  saveUser: function(userData) {
+    return axios.post("/api/signup", userData);
+  },
+  loginUser: function(email, password) {
+    return axios
+      .post("/api/login", {
+        email,
+        password
+      })
+      .then(function() {
+        console.log("logged in");
+        window.location.replace("/members");
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  },
+  getUser: function() {
+    return axios.get("/api/user_data");
+  },
+
+  getBills: function() {
+    return Promise.resolve({
+      data: [
+        {
+          id: 1,
+          name: "visa",
+          amount: 100,
+          dueDate: "2019-11-16",
+          paid: "yes"
+        },
+        {
+          id: 2,
+          name: "visa",
+          amount: 50,
+          dueDate: "2019-11-16",
+          paid: "no"
+        }
+      ]
+    });
+  }
+};
