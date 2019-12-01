7ab6b79c8e04ff701c35f5e804ba6911f5473934
gitMarcldo <marcldo@users.noreply.github.com>
2019-12-01 14:24:50 -0500

    fixed some formatting

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -37,7 +37,7 @@ class Home extends React.Component {
                 <div className="col-lg-12 text-center">
                   <h2 className="section-heading text-uppercase">Services</h2>
                   <h3 className="section-subheading text-muted">
-                    Personal finance app that helps you to stay on top of your bills.<br />
+                    A Personal finance app that helps you to stay on top of your bills.<br />
                     Monitoring your spending is a breeze with our cutting edge tools.
                   </h3>
                 </div>
@@ -49,8 +49,8 @@ class Home extends React.Component {
                   </span>
                   <h4 className="service-heading">Better Credit Score</h4>
                   <p className="text-muted">
-                    This app allows you to Add, Delete and Update your bills.
-                    Which eventually helps in maintaining Better Credit Score.
+                    One convenient app to add, delete and update your bills.
+                    Which eventually helps in maintaining better credit score.
                   </p>
                 </div>
                 <div className="col-md-4">
@@ -60,7 +60,7 @@ class Home extends React.Component {
                   <h4 className="service-heading">No Overdue Bills</h4>
                   <p className="text-muted">
                     This app is simple and easy to use which helps you see your
-                    bills status to avoid any unwanted Overdue Bills.
+                    bills status to avoid any unwanted overdue bills.
                   </p>
                 </div>
                 <div className="col-md-4">
@@ -69,8 +69,8 @@ class Home extends React.Component {
                   </span>
                   <h4 className="service-heading">Track Your Spending</h4>
                   <p className="text-muted">
-                    This app has Nice Visuals with graph and chart to maintain
-                    and Track Your Spending for better control over your finance.
+                    Our dashboard offers graphs and charts to monitor
+                    and track Your spending for better control over your finance.
                   </p>
                 </div>
               </div>
